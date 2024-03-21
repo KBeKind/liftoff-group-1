98491ea41bee6b5873dc5834ed2f66e88c110b06
Kyle Banderman <kyle.banderman4675@gmail.com>
2024-03-21 13:23:36 -0500

    more work on the frontend

--- backend/src/main/java/org/launchcode/liftoffgroup1/controllers/ApiController.java
+++ backend/src/main/java/org/launchcode/liftoffgroup1/controllers/ApiController.java
@@ -1,5 +1,6 @@
 package org.launchcode.liftoffgroup1.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,5 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class ApiController {
 
+    @GetMapping("/signUp")
+    public String displayUserSignUp () {
+        return "signUp/index";
+    }
 
 }
--- frontend/frontend/src/App.jsx
+++ frontend/frontend/src/App.jsx
@@ -2,34 +2,70 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { ReactDOM } from 'react-dom/client'
+import UserForm from './components/UserForm.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <UserForm />
     </>
   )
+}
+
+const React = require('react');
+const client = require('./client');
+
+class construct extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {users: []};
+  }
+
+  componentDidMount() {
+    client({method: 'GET', path: '/api/signUp'}).done(response => {
+      this.setState({users: response.entity._embedded.users});
+    });
+  }
+
+  render () {
+    return (
+      <UserList users={this.state.users}/>
+    )
+  }
+}
+
+class UserList extends React.Component {
+  render () {
+    const users = this.props.users.map(user =>
+        <User key={user._links.self.href} user={user}/>
+      );
+
+    return (
+      <table>
+        <tbody>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            {users}
+          </tr>
+        </tbody>
+      </table>
+    )
+  }
+}
+
+class User extends React.Component {
+  render() {
+    return (
+      <tr>
+        <td>{this.props.user.name}</td>
+        <td>{this.props.user.email}</td>
+      </tr>
+    )
+  }
+
 }
 
 export default App
