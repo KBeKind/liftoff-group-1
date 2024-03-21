0421be25745a70b891395d8f9743e484ccfd353a
Kyle Banderman <kyle.banderman4675@gmail.com>
2024-03-21 15:04:25 -0500

    almost have frontend and backend working i think

--- backend/src/main/java/org/launchcode/liftoffgroup1/controllers/ApiController.java
+++ backend/src/main/java/org/launchcode/liftoffgroup1/controllers/ApiController.java
@@ -1,16 +1,29 @@
 package org.launchcode.liftoffgroup1.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.launchcode.liftoffgroup1.model.User;
+import org.launchcode.liftoffgroup1.model.data.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.service.annotation.PostExchange;
 
 @RestController
 @RequestMapping("/api")
 public class ApiController {
 
-    @GetMapping("/signUp")
-    public String displayUserSignUp () {
-        return "signUp/index";
-    }
+    @Autowired
+    UserRepository userRepository;
 
+    @PostExchange("/users")
+    public User handleNewUserForm (@RequestParam String newUserRequest) {
+        User user = (User) (Object) newUserRequest;
+        userRepository.save(user);
+        return user;
+    }
+    //post
+    //get
+    //put
+    //delete
 }
---
+++ frontend/frontend/src/app/form/UserForm.js
@@ -0,0 +1,48 @@
+"use client";
+import React, {useState} from 'react';
+import axios from "axios";
+
+function UserForm() {
+    const [user, setUser] = useState({name: '', email: '' });
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        fetch('<http://localhost:8080/api/users>', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(user),
+        })
+        .then(response => response.json())
+        .then(data => console.log('User created:', data))
+        .catch(error => console.error('Error creating user:', error));
+    };
+
+    const handleChange = (event) => {
+        const {name, value} = event.target;
+        setUser(prevUser => ({ ...prevUser, [name]: value}));
+    };
+
+    return (
+    <form onSubmit={handleSubmit}>
+        <input 
+            type="text"
+            name="name"
+            value={user.name}
+            onChange={handleChange}
+            placeholder="Enter a Username:"
+        />
+        <input 
+            type="email"
+            name="email"
+            value={user.email}
+            onChange={handleChange}
+            placeholder="Enter an email:"
+        />
+        <button type="submit">Submit</button>
+    </form>
+    );
+}
+
+export default UserForm;
