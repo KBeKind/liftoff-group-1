51e5fa875848cc77a36d1dcde5a08111ab73aee0
Kyle Banderman <kyle.banderman4675@gmail.com>
2024-03-27 22:56:22 -0500

    added basic full crud to backend. frontend only has create. started added read to frontend

--- backend/src/main/java/org/launchcode/liftoffgroup1/controllers/ApiController.java
+++ backend/src/main/java/org/launchcode/liftoffgroup1/controllers/ApiController.java
@@ -1,14 +1,16 @@
 package org.launchcode.liftoffgroup1.controllers;
 
+import org.apache.coyote.Response;
+import org.launchcode.liftoffgroup1.model.FormUser;
 import org.launchcode.liftoffgroup1.model.User;
 import org.launchcode.liftoffgroup1.model.data.UserRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.service.annotation.PostExchange;
 
+@CrossOrigin("http://localhost:3000")
 @RestController
 @RequestMapping("/api")
 public class ApiController {
@@ -16,14 +18,46 @@ public class ApiController {
     @Autowired
     UserRepository userRepository;
 
-    @PostExchange("/users")
-    public User handleNewUserForm (@RequestParam String newUserRequest) {
-        User user = (User) (Object) newUserRequest;
-        userRepository.save(user);
-        return user;
+    @PostMapping("/users")
+    public ResponseEntity<String> createNewUser (@RequestBody FormUser newUserRequest) {
+        FormUser user = (FormUser) (Object) newUserRequest;
+        User newUser = new User();
+        BeanUtils.copyProperties(user, newUser);
+        userRepository.save(newUser);
+        return ResponseEntity.ok("User Created");
     }
-    //post
-    //get
-    //put
-    //delete
+
+    @GetMapping("/users/{id}")
+    public ResponseEntity<User> fetchUserData (@PathVariable int id) {
+        User fetchedUser = userRepository.findById(id).get();
+        return ResponseEntity.ok(fetchedUser);
+    }
+
+    @PutMapping("/users/{id}")
+    public ResponseEntity<String> updateUserData (@PathVariable int id, @RequestBody(required = false) String name, @RequestBody(required = false) String email) {
+        User fetchedUser = userRepository.findById(id).get();
+        if (name.isBlank() && email.isBlank()) {
+            return ResponseEntity.ok("No data entered");
+        } else if (!name.isBlank() && email.isBlank()) {
+            fetchedUser.setName(name);
+            return ResponseEntity.ok("Username changed");
+        } else if (name.isBlank() && !email.isBlank()) {
+            fetchedUser.setEmail(email);
+            return ResponseEntity.ok("Email changed");
+        } else {
+            fetchedUser.setName(name);
+            fetchedUser.setEmail(email);
+            return ResponseEntity.ok("Username and Email changed");
+        }
+    }
+
+    @DeleteMapping("/users/{id}")
+    public ResponseEntity<String> deleteUserData (@PathVariable int id, @RequestBody String verify) {
+        if (userRepository.findById(id).isPresent() && verify.equals("true")) {
+            userRepository.deleteById(id);
+            return ResponseEntity.ok("User successfully deleted");
+        } else {
+            return ResponseEntity.ok("User deletion denied");
+        }
+    }
 }
---
+++ backend/src/main/java/org/launchcode/liftoffgroup1/model/FormUser.java
@@ -0,0 +1,7 @@
+package org.launchcode.liftoffgroup1.model;
+
+public class FormUser {
+
+    private String name;
+    private String email;
+}
